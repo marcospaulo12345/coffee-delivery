a0d46fcc64aa0ddf94c0d1c26636cd63cea0c1e9
marcospaulo12345 <marcospaulo.mpf34@gmail.com>
2023-06-13 09:18:27 -0300

    config: Configuração inicial, adicionado o styled-components, layout, theme, global style e rotas

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-200': '#DBAC2C',
+  'yellow-300': '#C47F17',
+
+  'purple-100': '#EBE5F9',
+  'purple-200': '#8047F8',
+  'purple-300': '#4B2995',
+
+  'gray-900': '#272221',
+  'gray-800': '#403937',
+  'gray-700': '#574F4D',
+  'gray-600': '#8D8686',
+  'gray-500': '#D7D5D5',
+  'gray-400': '#E6E5E5',
+  'gray-300': '#EDEDED',
+  'gray-200': '#F3F2F2',
+  'gray-100': '#FAFAFA',
+}
